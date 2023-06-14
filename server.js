03e4a3eebbadc85780e4a63baade74e70dad1444
Disk <136144717+disk0023@users.noreply.github.com>
2023-06-15 06:25:55 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import * as shared from 'http';
 import os from 'os';
-import {context} from "./public/script.js"
+import {
+    context,
+    opcode
+} from "./public/script.js"
 
 const ips=d=>{
     let ip = {}
@@ -15,11 +18,12 @@ const ips=d=>{
 const ip = ips("lo")
 const port = 3000
 
-const memory = []
-
-const HTTP = shared.createServer(context)
-
-
-HTTP.listen(port,ip,()=>{
+opcode.memory = []
+opcode.send = {
+    jsonrpc:"2.0",
+    id:0,
+    result:0
+}
+const HTTP = shared.createServer(context).listen(port,ip,()=>{
 console.log(`server listing http://${ip}:${port}`)
 })
