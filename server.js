60c0c0eb2b70648df9ef99484e042a499f6541d5
Disk <136144717+disk0023@users.noreply.github.com>
2023-06-15 05:50:44 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import * as shared from 'http';
 import os from 'os';
-import * as fs from "fs";
 import {context} from "./public/script.js"
 
 const ips=d=>{
@@ -16,58 +15,10 @@ const ips=d=>{
 const ip = ips("lo")
 const port = 3000
 
-const cros = {
-'Access-Control-Allow-Origin': '*',
-'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, delete',
-'Access-Control-Max-Age': 2592000,
-};
-
 const memory = []
 
 const HTTP = shared.createServer(context)
 
-import WebSocket, { WebSocketServer } from 'ws';
-
-const wss = new WebSocketServer({
-  port: 8080,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      // See zlib defaults.
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-    // Other options settable:
-    clientNoContextTakeover: true, // Defaults to negotiated value.
-    serverNoContextTakeover: true, // Defaults to negotiated value.
-    serverMaxWindowBits: 10, // Defaults to negotiated value.
-    // Below options specified as default values.
-    concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024 // Size (in bytes) below which messages
-    // should not be compressed if context takeover is disabled.
-  }
-});
-
-// Event yang dipanggil saat koneksi WebSocket terhubung
-wss.on('connection', ws => {
-  console.log('Klien terhubung');
-
-  // Event yang dipanggil saat menerima pesan dari klien
-  ws.on('message', message => {
-    console.log('Menerima pesan:', message);
-
-    // Mengirim pesan balasan ke klien
-    ws.send('Terima kasih atas pesan Anda!');
-  });
-
-  // Event yang dipanggil saat koneksi WebSocket ditutup
-  ws.on('close', () => {
-    console.log('Klien terputus');
-  });
-});
 
 HTTP.listen(port,ip,()=>{
 console.log(`server listing http://${ip}:${port}`)
