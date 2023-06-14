3f5f51e89cb3ae3243c7acd76470a9cf3adc5a62
Disk <136144717+disk0023@users.noreply.github.com>
2023-06-14 22:30:47 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -24,18 +24,32 @@ const cros = {
 
 const memory = []
 
-const event = function(req,res){
-const {method,url,headres}=req
-res.output={
-    jsonrpc:'2.0',
-    id:0,
-    result:os.networkInterfaces()
-}
-res.writeHead(200,cros);
-res.end(JSON.stringify(res.output)); 
-}
-
 const HTTP = shared.createServer(context)
+
+import WebSocket, { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({
+  port: 8080,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed if context takeover is disabled.
+  }
+});
 
 HTTP.listen(port,ip,()=>{
 console.log(`server listing http://${ip}:${port}`)
