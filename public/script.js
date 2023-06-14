6fda4eade5ced0a3d15fbcf852f08ff3bdd9155a
Disk <136144717+disk0023@users.noreply.github.com>
2023-06-14 20:54:13 +0700

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,1 +1,19 @@
+import os from "os"
 
+const cors = {
+'Access-Control-Allow-Origin': '*',
+'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, delete',
+'Access-Control-Max-Age': 2592000,
+};
+export = {
+    context:function(req,res){
+        const {method,url,headres}=req
+        res.output={
+          jsonrpc:'2.0',
+          id:0,
+          result:os.networkInterfaces()
+        }
+        res.writeHead(200,cors);
+        res.end(JSON.stringify(res.output));
+    }
+}
